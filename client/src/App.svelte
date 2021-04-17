33c79b49ab42fabdf502b062fc7163d40c3fdebd
Yannic Brügger <yannic.bruegger@gmail.com>
2021-04-17 14:47:55 +0200

    Create basic componentes and styling

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+  import Entry from './components/Entry.svelte';
+  function navigate() {
+    console.log('Hello World');
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+ <h1>Test</h1>
+ <Entry type="file" on:click={navigate}></Entry>
+ <Entry></Entry>
+ <Entry></Entry>
+ <Entry></Entry>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ client/src/components/Entry.svelte
@@ -0,0 +1,29 @@
+<script type="ts">
+  export let type = "directory";
+  console.log(type);
+</script>
+
+<main on:click>
+  <span class="icon">{type == "directory" ? '📁' : '📄'}</span><span>Testfile.png</span>
+</main>
+
+<style>
+main {
+  width: 100%;
+  border-top: 1px solid var(--grey-4);
+  padding: 1em 1em;
+  transition: cubic-bezier(0.075, 0.82, 0.165, 1) .5s all;
+}
+
+main:last-child {
+  border-bottom: 1px solid var(--grey-4);
+}
+
+main:hover {
+  background-color: var(--grey-5);
+}
+
+.icon {
+  margin-right: 1em;
+}
+</style>

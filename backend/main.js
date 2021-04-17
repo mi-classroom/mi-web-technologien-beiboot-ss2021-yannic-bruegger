7cf6a7708339b25363efb5536ef633305b637b07
Yannic Brügger <yannic.bruegger@gmail.com>
2021-04-17 10:58:11 +0200

    Add exif metadata read functionality

--- backend/main.js
+++ backend/main.js
@@ -1,22 +1,42 @@
 require('dotenv').config();
 const fs = require('fs');
+const sharp = require('sharp');
+var exif = require('exif-reader');
+
 
 const TARGET_DIRECTORY = process.env.TARGET_DIRECTORY || './data';
 
-const prependEmoji = (emoji, text) => `${emoji} ${text}`;
-const displayDir = (dirname) => prependEmoji('📂', dirname);
-const displayFile = (filename) => prependEmoji('📄', filename);
+(async () => {
+  logDirectory();
+  logDirectory('/G_US_HAM_46-1987');
+  logDirectory('/G_US_HAM_46-1987/04_UV');
+  getMetadata('/G_US_HAM_46-1987/04_UV/G_US_HAM_46-1987_UV-001.tif.jpg');
+})();
 
-logDirectory();
-logDirectory('/G_US_HAM_46-1987', 1);
-logDirectory('/G_US_HAM_46-1987/04_UV', 2);
+function getAbsolutePath(relativePath){
+  return TARGET_DIRECTORY + (relativePath || '')
+}
 
+async function getMetadata(pathToFile){
+  const absolutePath = getAbsolutePath(pathToFile);
+  // TODO: Check if path is actually a file, not a directory.
+  const isFile = fs.existsSync(absolutePath);
 
-function logDirectory(relativePath, indentation) {
-  const files = fs.readdirSync(TARGET_DIRECTORY + (relativePath || ''), {withFileTypes: true});
+  if(isFile){
+    const metadata = exif(await (await sharp(absolutePath).metadata()).exif).image;
+    return metadata;
+  }
+}
+
+function logDirectory(relativePath) {
+  const prependEmoji = (emoji, text) => `${emoji} ${text}`;
+  const displayDir = (dirname) => prependEmoji('📂', dirname);
+  const displayFile = (filename) => prependEmoji('📄', filename);
+
+  const files = fs.readdirSync(getAbsolutePath(relativePath), {withFileTypes: true});
 
   files.forEach((file) => {
-    const string = " ".repeat(indentation*2) + (file.isDirectory() ? displayDir(file.name) : displayFile(file.name));
+    const string = (file.isDirectory() ? displayDir(file.name) : displayFile(file.name));
     console.log(string);
   });
 }

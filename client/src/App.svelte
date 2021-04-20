742e52eff497020fd5f983acc08903de5aaf6486
Yannic Brügger <yannic.bruegger@gmail.com>
2021-04-20 17:27:49 +0200

    Add preview component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,7 @@
 <script type="ts">
   import axios from "axios";
   import Entry from './components/Entry.svelte';
+  import Preview from './components/Preview.svelte';
 
   const baseUrl = 'http://192.168.2.106:3000';
   let items = [];
@@ -16,7 +17,8 @@
 
   async function getMetadata(from: string) {
     meta = {};
-    meta = (await axios.get(baseUrl + from)).data;
+    meta = {...(await axios.get(baseUrl + from)).data};
+    console.log(meta);
   }
 
   function getResource() : string {
@@ -35,16 +37,36 @@
 </script>
 
 <main>
-  <div>
+  <div class="nav">
     <div class="url">{url ? url : '/'}</div>
     {#if url && items.length > 0}<Entry name=".." type="top" on:click={() => {navigate(getResource())}}></Entry>{/if}
     {#each items as item}
     <Entry {...item} on:click={() => {item.type == 'directory' ? navigate(item.resource) : getMetadata(item.resource);}}></Entry>
     {/each}
   </div>
+  <div class="preview">
+    {#if Object.keys(meta).length > 0}
+      <Preview meta="{meta}"></Preview>
+    {/if}
+  </div>
 </main>
 
 <style>
+main {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 0.5fr 1.5fr;
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas: ". .";
+}
+
+.preview {
+  width: 100%;
+  background-color: var(--grey-0);
+  color: var(--grey-5);
+}
+
 .url {
   width: 100%;
   padding: 1em 1em;
---
+++ client/src/components/Preview.svelte
@@ -0,0 +1,27 @@
+<script type="ts">
+  export let meta : any;
+</script>
+
+<main>
+  <h1>{meta.Artist ?? ''}</h1>
+  <table>
+    <tr>
+      <th>Key</th>
+      <th>Value</th>
+    </tr>
+    {#each Object.keys(meta) as key}
+    <tr>
+      <td>{key}</td>
+      <td>{meta[key]}</td>
+    </tr>
+    {/each}
+  </table>
+</main>
+
+<style>
+main {
+  width: 100%;
+  height: 100%;
+  padding: 1em;
+}
+</style>
